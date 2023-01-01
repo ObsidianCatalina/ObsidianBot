fdfa971868767076d197479524e8e3f30a34bdfb
HanTools233 <hantools@foxmail.com>
2023-01-01 16:43:52 +0800

    修复：一个很答辩的问题

--- modules/linux.py
+++ modules/linux.py
@@ -18,38 +18,32 @@ channel = Channel.current()
     )
 )
 async def linux(app: Ariadne, group: Group):
-    r = random.randint(0, 5)
+    r = random.randint(1, 5)
     match r:
         case 1:
             await app.send_message(
                 group,
-
-                MessageChain(f"usr不等于user哦，而是'Unix Software Resource'的缩写"),
-
+                MessageChain(f"usr不等于user哦，而是'Unix Software Resource'的缩写")
             )
         case 2:
             await app.send_message(
                 group,
-                MessageChain(f"/etc目录存放的主要是配置文件，如用户信息、服务的启动脚本、常用服务的配置文件等"),
+                MessageChain(f"/etc目录存放的主要是配置文件，如用户信息、服务的启动脚本、常用服务的配置文件等")
             )
-
         case 3:
-
             await app.send_message(
                 group,
-                MessageChain(f"/bin目录存放的主要是系统命令，如sudo，普通用户和root用户都可以执行"),
+                MessageChain(f"/bin目录存放的主要是系统命令，如sudo，普通用户和root用户都可以执行")
             )
         case 4:
             await app.send_message(
                 group,
                 MessageChain(
-                    f"rm是一个神奇的命令，用于删除文件，文件夹，目录，甚至是/,-rf参数表示的是强制执行，如果想删除一个或者大多个文件（文件夹），可以使用:sudo rm -rf file1 file2 file3或者sudo rm -rf folder1 folder2 folder3等"),
+                    f"rm是一个神奇的命令，用于删除文件，文件夹，目录，甚至是/,-rf参数表示的是强制执行，如果想删除一个或者大多个文件（文件夹），可以使用:sudo rm -rf file1 file2 file3或者sudo rm -rf folder1 folder2 folder3等")
             )
         case 5:
             await app.send_message(
                 group,
-
                 MessageChain(
                     f"不同的系统拥有不同的包管理，如deb系统的包管理为apt，dpkg RHEL系统为yum，dnf（openSUSE为zypper），Arch系统主要为pacman等")
-
             )
