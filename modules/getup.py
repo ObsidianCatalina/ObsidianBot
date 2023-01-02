f5ed1788b7452a6414156a19433ba4a6e32b05cf
HanTools233 <hantools@foxmail.com>
2023-01-02 13:00:12 +0800

    草 什么寄吧

--- modules/getup.py
+++ modules/getup.py
@@ -10,7 +10,6 @@ channel = Channel.current()
 
 @channel.use(ListenerSchema(listening_events=[NudgeEvent]))
 async def getup(app: Ariadne, event: NudgeEvent):
-<<<<<<< HEAD
     if event.target == 2946761346:
         if event.context_type == "group":
             await app.send_group_message(
@@ -22,18 +21,3 @@ async def getup(app: Ariadne, event: NudgeEvent):
                 event.friend_id,
                 MessageChain("再戳打死你！")
             )
-=======
-    if event.supplicant== [
-2946761346
-]:
-     if event.context_type == "group":
-        await app.send_group_message(
-            event.group_id,
-            MessageChain(MessageChain([At(event.supplicant), Plain(" 再戳老娘就把你手指头掰断！！！！！！！！")]))
-        )
-    elif event.context_type == "friend":
-        await app.send_friend_message(
-            event.friend_id,
-            MessageChain("再戳打死你！")
-        )
->>>>>>> 55de2ca540584d3e5c18fb7deb2bea19f3051b0a
